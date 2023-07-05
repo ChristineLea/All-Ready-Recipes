1419f287a625c33a67c96abea8cf4104cb794a67
Christine Lea <christine87@live.com.au>
2023-07-05 19:24:48 +1000

    "add function to display saved recipes"

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,22 @@ let recipeId = new RecipeObject( // Sample object which will be created when rec
 );
 // This can then be used to display on CARD & ACCORDIAN as it will inherit the methods
 recipeId.displayCard();
+
+/* This code functionality will execute from the RECIPE SUGGESTIONS HTML page
+WHEN a user selects save/button/icon/heart (recipeIdBtn) it will reference the recipe by its id, save to local storage
+and display in in the SAVED RECIPE accordion. */
+
+// 1. Variable which will reference the save button icon of a given recipe ID
+let recipeIdBtn = $(".recipeIdBtn");
+// 2. Event when save button is clicked
+recipeIdBtn.on("click", function () {
+	// 3. DOM traversal up to parent element which will be the recipe container with the recipe ID
+	let recipeId = $(this).prev().parent().attr("id");
+	// 4. Pass recipeId as an argument to the addSavedRecipe() function
+	addSavedRecipe(recipeId);
+});
+function addSavedRecipe(id) {
+	id.displayAccordion(); // This will populate the HTML accordian using the recipe id as a reference to the recipe object
+
+	localStorage.setItem("recipeId", JSON.stringify(id));
+}
