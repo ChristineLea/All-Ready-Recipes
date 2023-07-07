080dedd253e7a3f0f0c60dcfc5c0ef6841be6b15
Christine Lea <christine87@live.com.au>
2023-07-07 18:30:45 +1000

    "adjust get request to display multiple ingred"

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,7 +1,10 @@
-// CALORIE NINJAS API
+// // CALORIE NINJAS API
 const API_KEY = "J4fyAOnwuXGtNt+PjYXPZg==g2QzfMyhXiwtkSjP";
 // test query
-let query = "mashed potatos and sausages";
+let query = "potato and sausages";
+
+// VALIDATION
+// convert kg to pounds
 
 // GET request
 $.ajax({
@@ -11,36 +14,38 @@ $.ajax({
 	contentType: "application/json",
 	success: function (result) {
 		// access returned data
-		const objData = result.items[0];
-		// save each data category to an object
-		let obj = {
-			name: objData.name,
-			servingSize: objData.serving_size_g,
-			calories: objData.calories,
-			protein: objData.protein_g,
-			sugar: objData.sugar_g,
-			fiber: objData.fiber_g,
-			sodium: objData.sodium_mg,
-			potassium: objData.potassium_mg,
-			cholesterol: objData.cholesterol_mg,
-			carbohydrates: objData.carbohydrates_total_g,
-			fat: objData.fat_total_g,
-			saturatedFat: objData.fat_saturated_g,
-		};
-		// pass the object to the function to display the data
+		const objData = result.items;
+		// iterate over returned objects for each ingredient requested
+		// for all of the items returned, save each value to an object to access and display data
+		for (const item of objData) {
+			let obj = {
+				name: item.name,
+				servingSize: item.serving_size_g,
+				calories: item.calories,
+				protein: item.protein_g,
+				sugar: item.sugar_g,
+				fiber: item.fiber_g,
+				sodium: item.sodium_mg,
+				potassium: item.potassium_mg,
+				cholesterol: item.cholesterol_mg,
+				carbohydrates: item.carbohydrates_total_g,
+				fat: item.fat_total_g,
+				saturatedFat: item.fat_saturated_g,
+			};
+			// pass the object for each of the ingredients to the function to display the data
 			displayData(obj);
+		}
 	},
 	// if error - log to console
 	error: function ajaxError(jqXHR) {
 		console.error("Error: ", jqXHR.responseText);
 	},
-	
 });
 // test to display/output to the user
 function displayData(obj) {
 	const body = $("body");
 	let list = $("<ul>");
-	let ingredient = obj.name;
+	let ingredient = obj.servingSize;
 	let lI = $("<li>");
 	lI.text(ingredient);
 	list.append(lI);
